9a831d09941e61725d42be8de31824ee8d1a72b8
Shinobu686 <choppu.business@gmail.com>
2021-03-23 14:28:28 +0900

    fix #1 .gitignoreファイルの作成

---
+++ SampleCountUp/SampleCountUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleCountUp
+//
+//  Created by 久富稜也 on 2021/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
